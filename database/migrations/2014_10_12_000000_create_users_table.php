538246c59b4e687b9d96065ed14a3918a076f0fe
Hazem abd el aleam <hazem29778865@gmail.com>
2018-11-30 13:32:39 +0200

    MIgration && Swoole

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
+            $table->boolean('isBanned')->default(false);
+            $table->boolean('verified')->default(false);
+            $table->string('mob');
+            $table->string('facebook')->default("");
+            $table->string('whatsapp')->default("");
             $table->rememberToken();
             $table->timestamps();
         });
