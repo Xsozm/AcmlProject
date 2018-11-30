0e0fd7c65b1b790bc4cd610f5827ffed55ddbead
Hazem abd el aleam <hazem29778865@gmail.com>
2018-11-30 18:20:14 +0200

    Models&&RElations

--- app/User.php
+++ app/User.php
@@ -3,11 +3,15 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
+
+
+
+
     use Notifiable;
 
     /**
@@ -27,4 +31,34 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    //Works perfectly
+    public function items(){
+        return $this->hasMany('App\Item','user_id','id');
+    }
+
+    public function verify_token(){
+        return $this->hasOne('App\Verify_Token','user_id','id');
+    }
 }
