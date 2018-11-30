b344358093cfd912a73712f6a4d68615fc61d618
Hazem abd el aleam <hazem29778865@gmail.com>
2018-11-30 16:02:17 +0200

    BackEndBase

---
+++ BackEnd/database/migrations/2018_07_28_030747_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('place');
+            $table->text('description')->default(null);
+            $table->boolean('found');// 1 found 0 lost
+            $table->boolean('confirmed')->default(false);
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
