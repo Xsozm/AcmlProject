9a243c1ec62a6a4d7604fed2d5a0d5a6dc885c5e
Mohamed Hesham <power_mody@hotmail.com>
2018-12-02 18:33:17 +0200

    Revert "Fixing/login"

--- front-end/src/reducers/notifications.js
+++
@@ -1,9 +0,0 @@
-import {FETCH_NOTIFICATIONS,ADD_NEW_NOTIFICATION} from '../actions/types';
-const INTIL_STATE = {notifications:[]}
-export default function(state=INTIL_STATE,action) {
-    switch(action.type){
-       case FETCH_NOTIFICATIONS: return {...state,notifications:action.payload}
-       case ADD_NEW_NOTIFICATION: return{...state,notifications:[...state.notifications,action.payload]} 
-       default : return state
-    }
-}
